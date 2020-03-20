5e4c9cafa8c3dbe7ba80ab02d0a38912ab1a0ea4
uncommonMath <uncommonMath>
2020-03-20 19:20:45 +0300

    load

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,47 @@
 import map.Coordinates;
-import map.Map;
-import social.User;
 import xmlSaver.XML;
+import xmlSaver.XmlDeserializer;
 import xmlSaver.XmlSerializer;
 
-
-import java.io.Console;
-import java.util.Date;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 @XML
 public class Main {
+    private static void run_ui(ConsoleUI ui) {
+        ui.user2.addFriend(ui.user1);
+        ui.user1.setCoordinates(new Coordinates(20, 10));
+        var scanner = new Scanner(System.in);
+        while (true) {
+            switch (scanner.nextLine()) {
+                case "1" -> ui.user1.checkIn(ui.map, ui.user1.getCoordinates());
+                case "2" -> XmlSerializer.saveXml(ui, "test.xml", ConsoleUI.registry);
+                case "3" -> ui = (ConsoleUI) XmlDeserializer.loadXml("test.xml", ConsoleUI.registry);
+                case "4" -> {
 
-    @XML
-    static User user1 = new User("Putin");
-    @XML
-    static User user2 = new User("Medvedev");
+                }
+                case "5" -> {
+                    var t = new Thread(ui::live);
+                    t.start();
+                }
+                case "6" -> {
+                    ui.users.forEach(System.out::println);
+                    var scan2 = new Scanner(System.in);
+                    var input2 = scan2.nextInt();
+                    ui.users.get(input2).setAlive(false);
+                    ui.users.remove(input2);
+                }
+                case "7" -> ui.users.forEach(x -> {
+                    System.out.println(x.getName() + "'s log:");
+                    x.getLog().forEach(System.out::println);
+                    System.out.println("\n");
+                });
+                default -> System.out.println("usage:(1|2|3|4|5|6|7)");
+            }
+        }
+    }
 
     public static void main(String[] args) {
         ConsoleUI c = new ConsoleUI();
-        c.run();
-
-//        System.out.println("\n" + user1.getLog().toString());
-//        System.out.println(user2.getNotifications().toString());
-    }
-
-    private static long kek(int n) {
-        long output;
-        if (n == 1 || n == 0) {
-            return 1;
-        }
-        //Recursion: Function calling itself!!
-        output = kek(n - 1) * n;
-        return output;
+        run_ui(c);
     }
 }
